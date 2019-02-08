41fe23a29bf1d4baaa6a6859855f7c18326acb04
Miles Steele <miles@milessteele.com>
2019-02-07 22:19:55 -0500

    render spectro

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate hound;
 extern crate stft;
+extern crate image;
 
 use stft::{STFT, WindowType};
 
@@ -17,6 +18,9 @@ fn main() {
     let mut spectrogram_column: Vec<f64> =
         std::iter::repeat(0.).take(stft.output_size()).collect();
 
+    let mut imgbuf = image::ImageBuffer::new(800, stft.output_size() as u32);
+    let mut img_x = 0;
+
     let mut out_spec = reader.spec().clone();
     out_spec.channels = 1;
     let mut writer = hound::WavWriter::create("tmp/out.wav", out_spec).unwrap();
@@ -25,11 +29,24 @@ fn main() {
         let sample: i32 = sample.unwrap();
         stft.append_samples(&[sample as f64]);
         while stft.contains_enough_to_compute() {
+            if img_x >= imgbuf.width() {
+                break
+            }
             stft.compute_column(&mut spectrogram_column[..]);
+            for (i, &sv) in spectrogram_column.iter().enumerate() {
+                let pixel = imgbuf.get_pixel_mut(img_x, i as u32);
+                *pixel = image::Rgb([
+                    (sv * 30.) as u8,
+                    (sv * 20.) as u8,
+                    (sv * 10.) as u8,
+                ]);
+            }
             stft.move_to_next_column();
+            img_x += 1;
         }
         // writer.write_sample(sample);
     }
+    imgbuf.save("tmp/out.png").unwrap();
     writer.finalize().unwrap();
 }
 
