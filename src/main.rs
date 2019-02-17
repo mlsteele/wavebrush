6b370453f7e920a0307dd86332ff6bdfde0ec0ec
Miles Steele <miles@milessteele.com>
2019-02-17 13:11:52 -0500

    crash 7

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn main() {
-    use glium::glutin;
+use glium::glutin;
 
-    #[allow(unused_mut)]
-    let mut events_loop = glutin::EventsLoop::new();
-    let context = glutin::ContextBuilder::new().with_vsync(true);
-    let builder = glutin::WindowBuilder::new()
-        .with_title("Crash dummy")
-        .with_dimensions(glutin::dpi::LogicalSize::new(200., 200.));
-    let _display = glium::Display::new(builder, context, &events_loop).unwrap();
+fn main() {
+    let events_loop = glutin::EventsLoop::new();
+    let context = glutin::ContextBuilder::new();
+    let builder = glutin::WindowBuilder::new();
+    match glium::Display::new(builder, context, &events_loop) {
+        Err(err) => println!("error building display: {:?}", err),
+        Ok(_display) => println!("displ-a-ok"),
+    }
 }
